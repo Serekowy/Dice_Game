9f9ba0c8ce202f9ff961280aea340c49d51ab557
Cheesy2137 <adrian997112999@gmail.com>
2021-06-08 01:01:13 +0200

    Add project files.

---
+++ Gra w kości/Class1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gra_w_kości
+{
+    class Class1
+    {
+        public static void info(string name)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Witaj {name}, w grze w kości, poniżej znajdziesz wszelkie informacje (wyświetlają się tylko przy pierwszym uruchomieniu programu).");
+            Console.WriteLine("Po rozpoczęciu rozgrywki gracz dostaje 1000 Adiks Coinów w skrócie AC. Zapamiętaj to, ponieważ AC będzie podstawową walutą w tej grze.");
+            Console.WriteLine();
+            Console.WriteLine("Gra polega na losowaniu (rzucaniu) kością razem z komputerem, jeżeli liczba oczek na kostce będzie wyższa niż ta od komputera, gracz zgarnia postawioną kwotę do portfela, w przeciwnym wypadku gracz traci walutę. Podczas gdy komputer i gracz wylosują tą samą liczbę, postawiona kwota wraca do gracza ;).");
+            Console.WriteLine();
+            Console.WriteLine("Nie musisz się również martwić, gdy będziesz chciał zakończyć rozgrywkę, ponieważ gra automatycznie się zapisuje oznacza to, że nie stracisz postępu w grze i będziesz mógł wrócić kiedy tylko będziesz chciał z tą samą ilością AC.");
+            Console.WriteLine();
+            Console.WriteLine("To na tyle w tym dosyć długim, ale jakże treściwym wstępie, mam nadzieję, że gra się spodoba :D");
+            Console.ResetColor();
+            Console.WriteLine("Aby rozpocząć grę naciśnij dowolny przycisk...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        public static void dice1()
+        {
+            Console.WriteLine("---------------");
+            Console.WriteLine("-             -");
+            Console.WriteLine("-             -");
+            Console.WriteLine("-      O      -");
+            Console.WriteLine("-             -");
+            Console.WriteLine("-             -");
+            Console.WriteLine("---------------");
+        }
+        public static void dice2()
+        {
+            Console.WriteLine("---------------");
+            Console.WriteLine("-          O  -");
+            Console.WriteLine("-             -");
+            Console.WriteLine("-             -");
+            Console.WriteLine("-             -");
+            Console.WriteLine("-  O          -");
+            Console.WriteLine("---------------");
+        }
+        public static void dice3()
+        {
+            Console.WriteLine("---------------");
+            Console.WriteLine("-          O  -");
+            Console.WriteLine("-             -");
+            Console.WriteLine("-      O      -");
+            Console.WriteLine("-             -");
+            Console.WriteLine("-  O          -");
+            Console.WriteLine("---------------");
+        }
+        public static void dice4()
+        {
+            Console.WriteLine("---------------");
+            Console.WriteLine("-  O       O  -");
+            Console.WriteLine("-             -");
+            Console.WriteLine("-             -");
+            Console.WriteLine("-             -");
+            Console.WriteLine("-  O       O  -");
+            Console.WriteLine("---------------");
+        }
+        public static void dice5()
+        {
+            Console.WriteLine("---------------");
+            Console.WriteLine("-  O       O  -");
+            Console.WriteLine("-             -");
+            Console.WriteLine("-      O      -");
+            Console.WriteLine("-             -");
+            Console.WriteLine("-  O       O  -");
+            Console.WriteLine("---------------");
+        }
+        public static void dice6()
+        {
+            Console.WriteLine("---------------");
+            Console.WriteLine("-  O   O   O  -");
+            Console.WriteLine("-             -");
+            Console.WriteLine("-             -");
+            Console.WriteLine("-             -");
+            Console.WriteLine("-  O   O   O  -");
+            Console.WriteLine("---------------");
+        }
+    }
+}
---
+++ Gra w kości/Class2.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gra_w_kości
+{
+    class Class2
+    {
+        public static int result(int pnumber, int pcnumber, int bet)
+        {
+            if (pnumber > pcnumber)
+            {
+                Console.WriteLine($"Wygrywasz! + {bet} AC");
+                return bet;
+            }
+            else if (pnumber < pcnumber)
+            {
+                Console.WriteLine($"Niestety, komputer wygrał - {bet} AC ;c");
+                return -bet;
+            }
+            else
+            {
+                Console.WriteLine("Remis! Adiks Coiny wracają do Ciebie");
+                return 0;
+            }
+        }
+        public static int dice(int rand)
+        {
+            switch (rand)
+            {
+                case 1:
+                    Class1.dice1();
+                    return 1;
+                case 2:
+                    Class1.dice2();
+                    return 2;
+                case 3:
+                    Class1.dice3();
+                    return 3;
+                case 4:
+                    Class1.dice4();
+                    return 4;
+                case 5:
+                    Class1.dice5();
+                    return 5;
+                case 6:
+                    Class1.dice6();
+                    return 6;
+                default:
+                    return 0;
+            }
+        }
+        public static int bet(int ac)
+        {
+            bool spr = false;
+            int bet = 0;
+
+            while (!spr)
+            {
+                Console.Write("Wpisz ilość Adiks Coinów: ");
+                string pos = Console.ReadLine();
+                spr = int.TryParse(pos, out bet);
+                if (bet < 50)
+                {
+                    Console.WriteLine("Minimalna stawka to 50AC.");
+                    spr = false;
+                }
+                if (bet > ac)
+                {
+                    Console.WriteLine("Nie masz tyle Adiks Coinów.");
+                    spr = false;
+                }
+                continue;
+            }
+            return bet;
+        }
+    }
+}
